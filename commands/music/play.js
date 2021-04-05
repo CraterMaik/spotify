715c9f1e96774dc58b574c520e1f85f9c736ba27
cratermaik <cratermaik@gmail.com>
2021-04-05 17:55:42 -0500

    Add loop Test play command

--- commands/music/play.js
+++ commands/music/play.js
@@ -1,9 +1,14 @@
 const { Utils: { urlParse } } = require('spotify-manager')
+const ytdl = require('ytdl-core');
 
 const { id } = urlParse("https://open.spotify.com/playlist/0YcgZpU7ZNvHz2EfIja8Pz")
 
 const commands = require('../../commands.js')
-
+const list = [
+        'https://www.youtube.com/watch?v=papuvlVeZg8',
+        'https://www.youtube.com/watch?v=PMivT7MJ41M',
+        'https://www.youtube.com/watch?v=qPTfXwPf_HM'
+      ];
 module.exports = class playCommand extends commands.Command {
   constructor(){
     super({
@@ -15,31 +20,53 @@ module.exports = class playCommand extends commands.Command {
     });
   }
   async execute(msg, args, discord, manager){
-    
+
     let station = msg.guild.channels.cache.find(c => c.type === 'voice');
 
-    if(!station) return msg.channel.send('No se encontro el canal inidicado.')
+    if (!station) return msg.channel.send('No se encontro el canal inidicado.')
 
-    station.join()
-      .then((conn) => {
-        msg.channel.send(`Conectado en ${conn.channel.name}`);
-        
+    async function play(song, conn) {
+      const stream = ytdl(song, {
+        filter: 'audioonly',
+        highWaterMark: 1 << 25,
+        quality: "highestaudio"
       })
-    
-    async function play(guild, song) {
-      const serverQueue = await queue.get(guid.id)
+
+     await conn.play(stream)
+         .on('finish', async () => {
+            await play(song, conn)
+
+           })
+           .on('error', (error) => console.log(error));
     }
 
+    async function handleVideo(song) {
+      try {
+        station.join()
+          .then(async (conn) => {
+            msg.channel.send(`Conectado en ${conn.channel.name}`);
+            
+            /* PlayList Spotify */
+            const PlayList = await manager.getPlaylist(id)
+            const tracks = await PlayList.tracks.items;
+            
+            //name, duration_ms, popularity
+            const listTracks = tracks.map((track) => track.track.name)
 
+            
+            await play(song, conn)
+            //   console.log(listTracks);
 
-    const PlayList = await manager.getPlaylist(id)
-    const tracks = await PlayList.tracks.items;
+          })
+          
+      } catch (error) {
+        msg.channel.send('Hubo un error de reproducción.')
 
-    //name, duration_ms, popularity
-    const listTracks = tracks.map((track) => track.track.name)
+      }
+    }
 
- //   console.log(listTracks);
- 
+    handleVideo('https://www.youtube.com/watch?v=rwkoXft0dXU')
+
   }
 
 }
