c82688a506fbca1f56ac8d7f9b99e97629893891
cratermaik <cratermaik@gmail.com>
2021-04-05 18:38:52 -0500

    Update play: function nextSong and handleMusic

--- commands/music/play.js
+++ commands/music/play.js
@@ -4,57 +4,45 @@ const ytdl = require('ytdl-core');
 const { id } = urlParse("https://open.spotify.com/playlist/0YcgZpU7ZNvHz2EfIja8Pz")
 
 const commands = require('../../commands.js')
-const list = [
+const playlist = [
         'https://www.youtube.com/watch?v=papuvlVeZg8',
         'https://www.youtube.com/watch?v=PMivT7MJ41M',
         'https://www.youtube.com/watch?v=qPTfXwPf_HM'
       ];
+
+let playNum = {};
 module.exports = class playCommand extends commands.Command {
   constructor(){
     super({
       name: 'play',
       aliases: [],
-      category: 'general',
+      category: 'music',
       priority: 9,
       permLvl: 0
     });
   }
-  async execute(msg, args, discord, manager){
+  async execute(msg, args, discord, manager, client){
 
     let station = msg.guild.channels.cache.find(c => c.type === 'voice');
 
     if (!station) return msg.channel.send('No se encontro el canal inidicado.')
 
-    async function play(song, conn) {
-      const stream = ytdl(song, {
-        filter: 'audioonly',
-        highWaterMark: 1 << 25,
-        quality: "highestaudio"
-      })
-
-     await conn.play(stream)
-         .on('finish', async () => {
-            await play(song, conn)
-
-           })
-           .on('error', (error) => console.log(error));
-    }
-
-    async function handleVideo(song) {
+    async function handleMusic(playlist) {
       try {
         station.join()
           .then(async (conn) => {
             msg.channel.send(`Conectado en ${conn.channel.name}`);
             
             /* PlayList Spotify */
-            const PlayList = await manager.getPlaylist(id)
-            const tracks = await PlayList.tracks.items;
+            const SplayList = await manager.getPlaylist(id)
+            const tracks = await SplayList.tracks.items;
             
             //name, duration_ms, popularity
             const listTracks = tracks.map((track) => track.track.name)
 
-            
-            await play(song, conn)
+            playNum[msg.guild.id] = 0;
+            await play(playlist, conn)
+            await setPresence(playNum[msg.guild.id])
             //   console.log(listTracks);
 
           })
@@ -65,8 +53,49 @@ module.exports = class playCommand extends commands.Command {
       }
     }
 
-    handleVideo('https://www.youtube.com/watch?v=rwkoXft0dXU')
+    async function play(playlist, conn) {
+      // test
+      console.log('Pista: '+playNum[msg.guild.id]);
+
+      const stream = ytdl(playlist[playNum[msg.guild.id]], {
+        filter: 'audioonly',
+        highWaterMark: 1 << 25,
+        quality: "highestaudio"
+      })
+
+     await conn.play(stream)
+         .on('finish', async () => {
+           await nextSong(playlist, conn)
+            await setPresence(playNum[msg.guild.id])
+
+           })
+           .on('error', (error) => console.log(error));
+    }
+
+    async function nextSong(playlist, conn) {
+
+      playNum[msg.guild.id] = playNum[msg.guild.id] < playlist.length - 1 ? playNum[msg.guild.id] + 1 : 0;
+
+      await play(playlist, conn)
+
+    }
+
+    async function setPresence(song) {
+       client.user.setPresence({
+         activity: {
+           name: `Pista: ${song}`,
+           type: 'LISTENING'
+         }
+       })
+
+    }
+
+    handleMusic(playlist)
+
+    
 
   }
+  
+  
 
 }
