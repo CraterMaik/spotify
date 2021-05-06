d7c809f4443bbd6cefa4ea966937ecdb2bc51b7f
cratermaik <cratermaik@gmail.com>
2021-05-05 22:00:29 -0500

    Update test command and events

---
+++ commands/general/reminder.js
@@ -0,0 +1,18 @@
+const commands = require('../../commands.js')
+
+module.exports = class helpCommand extends commands.Command {
+  constructor() {
+    super({
+      name: 'reminder',
+      aliases: ['rem'],
+      category: 'general',
+      priority: 9,
+      permLvl: 3
+    });
+  }
+  execute(msg, args, discord) {
+    msg.channel.send('Reacciona este mensaje para el recordatorio.').then((message) => {
+       message.react(`⚪`)
+    })
+  }
+}
---
+++ events/messageReactionAdd.js
@@ -0,0 +1,20 @@
+
+module.exports = async (client, reaction, user) => {
+  const message = reaction.message;
+  if (user.bot) return;
+
+  if (reaction.partial) {
+    try {
+      await reaction.fetch();
+    } catch (error) {
+      console.log('Something went wrong when fetching the message: ', error);
+      return;
+    }
+  }
+ 
+  if (reaction.emoji.name === "⚪") {
+      const member = message.guild.members.cache.get(user.id);
+     // console.log(member);
+      message.guild.channels.cache.get(message.channel.id).send(`${member.user.username} (${member.user.id})`)
+  }
+}
